ce62e0d7841f688a243d3078fafb8514e0f5b2d9
Vsevolod Shchipunov <mastrogg@gmail.com>
2017-06-17 00:40:44 +0300

    style#1: Added .gitignore; removed obsolete version of project

--- backendtrying/mysite/polls/admin.py
+++ backendtrying/mysite/polls/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from polls.models import Recipe, Food, Choice
+admin.site.register(Food)
+admin.site.register(Choice)
 
-# Register your models here.
+
+class RecipeAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            'Date information',
+            {
+                'fields': ['pub_date'],
+            },
+        ),
+        (
+            'Recipe itself',
+            {
+                'fields': ['recipe_text'],
+            },
+        ),
+    ]
+
+
+admin.site.register(Recipe, RecipeAdmin)
--- backendtrying/mysite/polls/models.py
+++ backendtrying/mysite/polls/models.py
@@ -1,24 +1,25 @@
-from django.db import models
 import datetime
 
 from django.db import models
 from django.utils import timezone
 
 
-
 class Recipe(models.Model):
     recipe_text = models.CharField(max_length=400)
     pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.recipe_text
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
+
 class Food(models.Model):
     recipe = models.ForeignKey(Recipe)
     food_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
     def __str__(self):
         return self.food_text
 
@@ -26,6 +27,6 @@ class Food(models.Model):
 class Choice(models.Model):
     food = models.ForeignKey(Food)
     choice_text = models.CharField(max_length=200)
+
     def __str__(self):
         return self.choice_text
-
