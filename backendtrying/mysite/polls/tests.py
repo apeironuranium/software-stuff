c85d7dab7ad49ab55440cfcac5f644253de5726a
apeironuranium <lera.leroi@gmail.com>
2017-06-27 01:51:12 +0300

    tests try

--- backendtrying/mysite/polls/tests.py
+++ backendtrying/mysite/polls/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from polls.models import Food
+from polls.models import Recipe
+from polls.models import RecipeLine
 
-# Create your tests here.
+class TestFoodString(TestCase):
+    def setUp(self):
+        Food.objects.create(food_text = "Valeriia", pub_date = "1993-06-29")
+
+    def test_str_repr(self):
+        title = "Valeriia"
+        food_text = Food.objects.get(food_text=title)
+        self.assertEqual(str(food_text), title)
+
+
+class TestRecipeString(TestCase):
+   def setUp(self):
+       RecipeLine.objects.create(ingridient = "Oguretc")
+
+   def test_str_repr(self):
+       title = "Oguretc"
+       ingridient = Food.objects.get(food_text=title)
+       self.assertEqual(str(ingridient), title)
